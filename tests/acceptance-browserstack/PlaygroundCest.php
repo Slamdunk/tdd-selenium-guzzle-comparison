6a9b978a189900121d3d8fef2df8844abafb6e97
Filippo Tessarotto <zoeslam@gmail.com>
2018-04-16 11:17:06 +0200

    BrowserStack sample

---
+++ tests/acceptance-browserstack/PlaygroundCest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\AcceptanceSelenium;
+
+use AcceptanceBrowserstackTester;
+
+class PlaygroundCest
+{
+    public function testDatePickerWorks(AcceptanceBrowserstackTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('Playground PHP', 'h1');
+        $I->dontSee('19/04/2018', '#result');
+
+        $I->click('#date');
+        $I->click('//td/a[text()="19"]');
+        $I->click('Send');
+        $I->see('19/04/2018', '#result');
+    }
+}
